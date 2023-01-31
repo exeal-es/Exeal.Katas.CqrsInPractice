8cc5c438a65b96e791360b8544309e55bdc17e07
Pedro Pardal <ppardalj@gmail.com>
2023-01-31 14:19:55 +0100

    chore: add swagger

--- Before/src/Api/Startup.cs
+++ Before/src/Api/Startup.cs
@@ -21,10 +21,21 @@ namespace Api
 
             services.AddSingleton(new SessionFactory(Configuration["ConnectionString"]));
             services.AddScoped<UnitOfWork>();
+            
+            services.AddSwaggerGen();
         }
 
         public void Configure(IApplicationBuilder app)
         {
+            // This middleware serves generated Swagger document as a JSON endpoint
+            app.UseSwagger();
+
+            // This middleware serves the Swagger documentation UI
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "Students API V1");
+            });
+            
             app.UseMiddleware<ExceptionHandler>();
             app.UseRouting();
             app.UseEndpoints(builder => builder.MapControllers());
